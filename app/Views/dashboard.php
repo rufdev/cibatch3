155f665829a05bae5ce5d0bcb42ae4550189caa4
Rufino John <aguilarufino@gmail.com>
2023-11-15 15:49:21 +0800

    added dashboard

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -1,5 +1,116 @@
 <?= $this->extend('template/admin_template'); ?>
 
 <?= $this->section('content'); ?>
-<h1>DASHBOARD</h1>
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0">Dashboard</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-6 col-6">
+                <!-- small box -->
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3><?= $totalauthors ?></h3>
+
+                        <p>Total Authors</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-person"></i>
+                    </div>
+                </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-lg-6 col-6">
+                <!-- small box -->
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h3><?= $totalposts ?></h3>
+
+                        <p>Total Post</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-ios-list"></i>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+            <div class="card card-success">
+              <div class="card-header">
+                <h3 class="card-title">Bar Chart</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                    <i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove">
+                    <i class="fas fa-times"></i>
+                  </button>
+                </div>
+              </div>
+              <div class="card-body">
+                <div class="chart">
+                  <canvas id="barChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                </div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?= $this->endSection(); ?>
+
+<?= $this->section('pagescript'); ?>
+<script>
+    $(function(){
+        let barChartCanvas = $("#barChart").get(0).getContext("2d");
+        let barchartdata = <?= json_encode($barchartdata) ?>;
+
+        let barChartData =  {
+            labels : barchartdata.map(function(item){
+                return item.author_name;
+            }), // ['RUFINO JOHN', 'JADE HOYOS']
+            datasets: [{
+                label: 'Total Post',
+                backgroundColor: '#00a65a',
+                borderColor: '#00a65a',
+                borderWidth: 1,
+                data: barchartdata.map(function(item){
+                    return item.post_count;
+                }) //[10, 20]
+            }]
+        }
+
+        let barChartOptions = {
+            responsive: true,
+            maintainAspectRatio: false,
+            datasetFill: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        stepSize: 1
+                    }
+                }]
+            }
+        }
+
+        new Chart(barChartCanvas, {
+            type: 'bar',
+            data: barChartData,
+            options: barChartOptions
+        });
+    });
+</script>
 <?= $this->endSection(); ?>
