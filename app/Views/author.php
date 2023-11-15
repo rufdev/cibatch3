b007c252d26966879f92ef493efcb3c725625eb0
Rufino John <aguilarufino@gmail.com>
2023-11-15 14:13:10 +0800

    added modal

--- app/Views/author.php
+++ app/Views/author.php
@@ -15,6 +15,13 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-md-12">
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalID">
+                    Add Author
+                </button>
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col-md-12">
                 <table id="dataTable" class="table table-bordered table-striped">
                     <thead>
                         <tr>
@@ -29,7 +36,27 @@
                 </table>
             </div>
         </div>
-
+        <div class="modal fade" id="modalID">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Default Modal</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <p>One fine body&hellip;</p>
+                    </div>
+                    <div class="modal-footer justify-content-between">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+                <!-- /.modal-content -->
+            </div>
+            <!-- /.modal-dialog -->
+        </div>
     </div>
 </section>
 <?= $this->endSection(); ?>
